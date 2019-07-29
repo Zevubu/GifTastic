bc7923114377744cf8b7d9f9a6a23a46334d5297
Zev Hoffman <zevubu@gmail.com>
2019-07-28 19:51:29 -0700

    Buit html assests javascript and css. added gif API key and added it to git ignore. wrote out plan for my js

---
+++ assets/javascript/producer.js
@@ -0,0 +1,25 @@
+// window on load
+
+// Declair variables.
+
+
+// Get element by id button area.
+// Create for loop to make buttons inside of button area with an attribute of the search name. The text of the button is connected to the name.  
+
+// Import GIFY api.
+// Run fetch function to pull api in.
+// Build section box for each gif then apply the gif to it and then add gif info to it.
+// add button to add it to favorite box.
+
+// bonus
+
+// build area for favorite gifs
+
+// Import news api.
+// Run fetch function to pull api in.
+
+// build area for favorite articles
+
+// figure out how to make horizantal scrolling sections to hold gifs and articles.
+
+
