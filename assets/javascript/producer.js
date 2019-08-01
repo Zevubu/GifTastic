f085f7e4133a8129f4c1786b9684e3c236cea4b3
Zev Hoffman <zevubu@gmail.com>
2019-07-31 23:51:42 -0700

    attempting to implimanet a pause when clicked function.

--- assets/javascript/producer.js
+++ assets/javascript/producer.js
@@ -3,15 +3,14 @@ window.onload = function (){
 
     // Declair variables.
     let specialNames =["random", "trending"]
-    let searchNames = ["Dune", "Klaus Nomi","Iggy Pop"]
+    let searchNames = ["Dune", "Klaus Nomi","Iggy Pop","Tank Girl"]
     let gifButton;
     let gifCount = 0;
+    let gifNum = 12;
     let currentTitle;
-    let spCurrentTitle;
     let currentGif;
     let result;
     let newDivBox;
-    let divBoxCount = 0;
     let newDiv;
     let newGif;
     let divBox0;
@@ -19,14 +18,86 @@ window.onload = function (){
 
     let spButtonArea = document.getElementById("special-button-area")
     let buttonArea = document.getElementById("button-area");
-    let allButton = document.getElementsByTagName("button");
+    
     let gifArea = document.getElementById("gif-area");
     let articleArea = document.getElementById("article-area");
     let addGifBTN = document.getElementById("add-gif")
     let gifInput = document.getElementById("gif-input")
+    let gifTag = document.getElementsByTagName("img")
     // gifInput = this.getAttribute("value");
     
     
+let initializSearch = function(){
+
+
+        let allButton = document.getElementsByTagName("button");
+         console.log(`number of buttons: ${allButton.length}`);
+        for (i = 0; i < allButton.length; i++){
+            console.log("for loop")
+            allButton[i].addEventListener("click",function(){
+                gifArea.innerHTML = " ";
+                gifCount = 0;
+                let queryURL = " ";
+                console.log("button clicked")
+                currentTitle = this.getAttribute("gif-id");
+                
+                queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${GIFKEY}&q=${currentTitle}&limit=${gifNum}&offset=0&rating=R&lang=en`
+            
+                fetch(queryURL).then(function(response){
+                    return response.json()
+                }).then(function(res){
+                    console.log(res)
+                    result = res.data;
+                    for( i = 0; i < result.length; i++){
+                        
+                        currentGif = result[i].images.fixed_height.url;
+                        newDiv = document.createElement("div");
+                        newDiv.setAttribute("class", "new-gif")
+                        newGif = document.createElement("img");
+                        newGif.setAttribute("src", currentGif);
+                        newGif.setAttribute("pause-play", "pause");
+                        newGif.setAttribute("class", "gif pause");
+                        newDiv.appendChild(newGif);
+                        if (gifCount < gifNum){
+
+                            if(gifCount === 0){
+                                newDivBox =  document.createElement("div");
+                                newDivBox.setAttribute("id", `gif-box-0`);
+                                gifArea.appendChild(newDivBox);
+                            }
+                            else if(gifCount%4 === 0){
+                                newDivBox =  document.createElement("div");
+                                newDivBox.setAttribute("id", `gif-box-${gifCount}`);
+                                gifArea.appendChild(newDivBox);                            
+                            };
+                            divBox0 = document.getElementById("gif-box-0");
+                            divBox5 = document.getElementById("gif-box-4");
+                            divBox10 = document.getElementById("gif-box-8");
+                            divBox15 = document.getElementById("gif-box-12");
+                            divBox20 = document.getElementById("gif-box-16");
+                            if (gifCount < 4){
+                                divBox0.appendChild(newDiv);
+                            }
+                            else if (gifCount < 8){
+                                divBox5.appendChild(newDiv);
+                            }
+                            else if (gifCount < 12){
+                                divBox10.appendChild(newDiv);
+                            }
+                            else if (gifCount < 16){
+                                divBox15.appendChild(newDiv);
+                            }
+                            else if (gifCount < 20){
+                                divBox20.appendChild(newDiv);
+                            }
+                            gifCount++;
+                        }
+                    }
+                })
+            })
+        }
+    }
+   
 
 
     // <section id="button-area"></section>
@@ -56,14 +127,17 @@ window.onload = function (){
             gifButton.setAttribute("gif-id", searchNames[i]);
             gifButton.textContent = (searchNames[i]);
             buttonArea.append(gifButton);
+            initializSearch();
+           
             // it works!
         }
     };
     btnMaker();
-    console.log(`number of buttons: ${allButton.length}`);
+   
 
     addGifBTN.addEventListener("click", function(){
         event.preventDefault()
+        buttonArea.innerHTML = ("");
         console.log(gifInput.value)
         // gifInput.setAttribute("name", this.nodeName);
         let newGif = gifInput.value;
@@ -74,78 +148,21 @@ window.onload = function (){
 
     });
 
+ gifTag.addEventListener("click", function(){
+        console.log("GIF clicked")
+        let gifPauser = this.getAttribute("pause-play");
+        if(gifPauser === "pause"){
+            this.setAttribute("pause-play", "play")
+            this.setAttribute("class", "gif play")
 
-    for (i = 0; i < allButton.length; i++){
-        console.log("for loop")
-        allButton[i].addEventListener("click",function(){
-            gifArea.innerHTML = " ";
-            gifCount = 0;
-            let queryURL = " ";
-            console.log("button clicked")
-            currentTitle = this.getAttribute("gif-id");
-            
-            queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${GIFKEY}&q=${currentTitle}&limit=25&offset=0&rating=G&lang=en`
-           
-
-            fetch(queryURL).then(function(response){
-                return response.json()
-            }).then(function(res){
-                console.log(res)
-                result = res.data;
-                for( i = 0; i < result.length; i++){
-                    
-                    currentGif = result[i].images.fixed_height.url;
-                    newDiv = document.createElement("div");
-                    newDiv.setAttribute("class", "new-gif")
-                    newGif = document.createElement("img");
-                    newGif.setAttribute("src", currentGif);
-                    newDiv.appendChild(newGif);
-
-                    if(gifCount === 0){
-                        newDivBox =  document.createElement("div");
-                        newDivBox.setAttribute("id", `gif-box-0`);
-                        gifArea.appendChild(newDivBox);
-                    }
-                    else if(gifCount%5 === 0){
-                        newDivBox =  document.createElement("div");
-                        newDivBox.setAttribute("id", `gif-box-${gifCount}`);
-                        gifArea.appendChild(newDivBox);
-                        
-                    };
-                    divBox0 = document.getElementById("gif-box-0");
-                    divBox5 = document.getElementById("gif-box-5");
-                    divBox10 = document.getElementById("gif-box-10");
-                    divBox15 = document.getElementById("gif-box-15");
-                    divBox20 = document.getElementById("gif-box-20");
-                    if (gifCount < 5){
-                        divBox0.appendChild(newDiv);
-                    }
-                    else if (gifCount < 10){
-                        divBox5.appendChild(newDiv);
-
-                    }
-                    else if (gifCount < 15){
-                        divBox10.appendChild(newDiv);
-
-                    }
-                    else if (gifCount < 20){
-                        divBox15.appendChild(newDiv);
-
-                    }
-                    else if (gifCount < 25){
-                        divBox20.appendChild(newDiv);
-
-                    }
-                    gifCount++;
-                    
-
-                }
-            })
+        }
+        else{
+            this.setAttribute("pause-play", "pause")
+            this.setAttribute("class", "gif pause")
+        }
+    })
 
 
-
-        })
-    }
     
     // Import GIFY api.
 
